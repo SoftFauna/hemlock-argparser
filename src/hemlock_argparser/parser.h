da06cf72a700b652c94ef00517f84e6102426157
sage-etcher <sage.codes@email.com>
2025-03-11 09:02:08 -0400

    fix more empty header guards

--- src/hemlock_argparser/parser.h
+++ src/hemlock_argparser/parser.h
@@ -3,8 +3,8 @@
  * Copyright (c) 2025 The SoftFauna Team
  * MIT License */
 
-#ifndef HMARG__HEADER
-#define HMARG__HEADER
+#ifndef HMARG_PARSER_HEADER
+#define HMARG_PARSER_HEADER
 #ifdef __cplusplus
 extern "C" {
 #endif
