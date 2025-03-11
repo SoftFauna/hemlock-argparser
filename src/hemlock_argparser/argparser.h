756b488afb2cd406d75dbe23ff4dab10ad78d5fb
sage-etcher <sage.codes@email.com>
2025-03-11 14:41:24 -0400

    added javadoc comments

--- src/hemlock_argparser/argparser.h
+++ src/hemlock_argparser/argparser.h
@@ -3,6 +3,11 @@
  * Copyright (c) 2025 The SoftFauna Team
  * MIT License */
 
+/**
+ * @file
+ * document me.
+ */
+
 #ifndef HMARG_ARGPARSER_HEADER
 #define HMARG_ARGPARSER_HEADER
 #ifdef __cplusplus
@@ -30,20 +35,154 @@ extern "C" {
 #   endif
 #endif
 
-/* api */
-LIB_HEMARG_API int copt_parser (copt_t *option_array, size_t option_count, char **argv, size_t argc, void *cb_data);
 
-LIB_HEMARG_API char *copt_print_options (copt_t *option_array, size_t option_count, int wrap_col);
+/**
+ * Completely proccess a program's argv+argc, against a predefined set of
+ * options. The option_array is a pointer array of 
+ * [copt_t](#copt_t) objects defining all valid options. 
+ * The option_count is the length of option_array. cb_data is a void* passed 
+ * as data to an option's callback function.
+ * 
+ * @param option_array an array of supported options/flags.
+ * @param option_count length of option_array.
+ * @param argv         arguement list.
+ * @param argc         arguement count.
+ * @param cb_data      a void* to pass to any callback functions.
+ * 
+ * @return 0 on a success, -1 on failure.
+ * 
+ * @see copt_t
+ */
+LIB_HEMARG_API int copt_parser (copt_t *option_array, size_t option_count, 
+        char **argv, size_t argc, void *cb_data);
 
+
+/**
+ * Given an array of copt_ts, option_array, generate a program's option 
+ * documentation. The resulting print-optimized string is heap allocated; 
+ * caller takes responisibility for cleanup. wrap_col specifies the column at 
+ * which the option/flag's descriptions should wrap. if wrap_col is 0 no pretty
+ * wrapping will occur. If color is enabled, the generated string will use 
+ * ANSI escape codes to print beautifully.
+ * 
+ * options's use format:
+ * ```text
+ *  -d, --description-example    description line one. continue the first line 
+ *                                 and then pretty wrap description to 80
+ *                                 columns.
+ *  -a                           short flag.
+ *  -b=<parameter>               short input option.
+ *      --long_a                 long flag.
+ *      --long_b=<parameter>     long input option.
+ *  -c, --long_c                 short and long flags.
+ *  -d, --long_d=<parameter>     short and long input option.
+ * ```
+ * 
+ * @param option_array an array of options/flags to be parsed.
+ * @param option_count the length of option_array.
+ * @param wrap_col     the column to pretty wrap outputs to.
+ * 
+ * @return a caller-owned heap-allocated string documenting what options are 
+ *           provided by option_array.
+ * 
+ * @see copt_t
+ * @see copt_enable_color_cb()
+ * @see copt_disable_color_cb()
+ */
+LIB_HEMARG_API char *copt_print_options (copt_t *option_array, 
+        size_t option_count, int wrap_col);
+
+
+/**
+ * Given a set of program information, copt_prog_t, generate a program's help 
+ * page. The resulting print-optimized string is heap allocated; caller takes 
+ * responisibility for cleanup. wrap_col specifies the column at which the 
+ * option/flag's descriptions should wrap. if wrap_col is 0 no pretty wrapping 
+ * will occur. If color is enabled, the generated string will use ANSI escape 
+ * codes to print beautifully.
+ * 
+ * @param program  some basic information about the program: name, 
+ *                   license, accepted options, ect..
+ * @param wrap_col the column to pretty wrap output to.
+ * 
+ * @return a caller-owned heap-allocated string documenting the program's
+ *           help page.
+ * 
+ * @see copt_prog_t
+ * @see copt_enable_color_cb()
+ * @see copt_disable_color_cb()
+ */
 LIB_HEMARG_API char *copt_help_page (copt_prog_t *program, int wrap_col);
 
+
+/**
+ * Given a set of program information, copt_prog_t, generate a program's 
+ * version page. The resulting print-optimized string is heap allocated; 
+ * caller takes responisibility for cleanup. If color is enabled, the 
+ * generated string will use ANSI escape codes to print beautifully.
+ * 
+ * @param program  some basic information about the program: name, 
+ *                   license, accepted options, ect..
+ * 
+ * @return a caller-owned heap-allocated string documenting the program's 
+ *           version page.
+ * 
+ * @see copt_prog_t
+ * @see copt_enable_color_cb()
+ * @see copt_disable_color_cb()
+ */
 LIB_HEMARG_API char *copt_version_page (copt_prog_t *program);
 
-LIB_HEMARG_API int copt_enable_colors_cb (copt_t *option_array, size_t option_count, void *cb_data);
 
-LIB_HEMARG_API int copt_disable_colors_cb (copt_t *option_array, size_t option_count, void *cb_data);
-
+/**
+ * Check if colors are enabled/disabled. Return a boolean. 
+ * 
+ * @return 0 if disabled, 1 if enabled.
+ * 
+ * @see copt_enable_colors_cb()
+ * @see copt_disable_colors_cb()
+ */
 LIB_HEMARG_API int copt_are_colors_enabled (void);
+
+
+/**
+ * A callback for enabling coloured printing of error messages, help page, 
+ * and version page generation. Used in conjunction with copt_t CALLBACK, 
+ * enables colors when a user defined option is encountered.
+ * 
+ * @param option_array ignored
+ * @param option_count ignored
+ * @param cb_data      ignored
+ * 
+ * @return COPT_CB_SUCCESS or COPT_CB_FAILURE depending on success/failure.
+ * 
+ * @see copt_t 
+ * @see copt_cb_t
+ * @see copt_cb_return_t
+ * @see copt_parser()
+ */
+LIB_HEMARG_API int copt_enable_colors_cb (copt_t *option_array, 
+        size_t option_count, void *cb_data);
+
+
+/**
+ * A callback for diabling coloured printing of error messages, help page, 
+ * and version page generation. Used in conjunction with copt_t CALLBACK, 
+ * disables colors when a user defined option is encountered.
+ * 
+ * @param option_array ignored
+ * @param option_count ignored
+ * @param cb_data      ignored
+ * 
+ * @return COPT_CB_SUCCESS or COPT_CB_FAILURE depending on success/failure.
+ * 
+ * @see copt_t 
+ * @see copt_cb_t
+ * @see copt_cb_return_t
+ * @see copt_parser()
+ */
+LIB_HEMARG_API int copt_disable_colors_cb (copt_t *option_array, 
+        size_t option_count, void *cb_data);
 
 
 #ifdef __clplusplus
--- src/hemlock_argparser/option.h
+++ src/hemlock_argparser/option.h
@@ -3,6 +3,12 @@
  * Copyright (c) 2025 The SoftFauna Team
  * MIT License */
 
+/**
+ * @file
+ * Define every typedef, structure, and enum necessary for propper use of 
+ * the Hemlock Arguement Parser.
+ */
+
 #ifndef HMARG_OPTION_HEADER
 #define HMARG_OPTION_HEADER
 #ifdef __cplusplus
@@ -11,54 +17,133 @@ extern "C" {
 
 #include <stddef.h>
 
+
+/* this is required, otherwise when we get to defining copt_cb_t, the copt_t 
+ * type will be missing and we will get all kinds of errors */
+typedef struct copt copt_t;
+
+
+/**
+ * option type enumerator, paired with copt specifies which type of 
+ * variable/callback to write to. 
+ * 
+ * @see copt_return_t
+ * @see copt_t
+ * @see copt_parser()
+ */
 typedef enum
 {
-    CONARG_TYPE_NULL = 0,
-    INPUT_INT,
-    INPUT_STR,
-    FLAG_T,
-    FLAG_F,
-    CALLBACK,
-    CONARG_TYPE_CNT,
+    CONARG_TYPE_NULL = 0,   /**< no type specified, type error      */
+    INPUT_INT,              /**< specifys a return type of int*     */
+    INPUT_STR,              /**< specifys a return type of char**   */
+    FLAG_T,                 /**< specifys a return type of int*     */
+    FLAG_F,                 /**< specifys a return type of int*     */
+    CALLBACK,               /**< specifys a callback function       */
+    CONARG_TYPE_CNT,        /**< total number of types, counter     */
 } copt_type_t;
 
+
+/**
+ * Defines an option callback function. executed by copt_parser, if a 
+ * matching arguement is reached and the option is of type CALLBACK, the 
+ * paired callback funtion is executed on the spot.
+ * 
+ * The callback function is expected to take the option_array, option_count, 
+ * and some abstract cb_data, each of which are provided by the user when 
+ * calling copt_parser.
+ * 
+ * Additionally, a return of COPT_CB_SUCCESS is indicitive of a success, and 
+ * COPT_CB_FAILURE of a failure.
+ * 
+ * @see copt_cb_return_t
+ * @see copt_type_t
+ * @see copt_t
+ * @see copt_parser()
+ */
+typedef int (*copt_cb_t)(copt_t *option_arr, size_t option_cnt, void *cb_data);
+
+
+/**
+ * Returned value of copt_cb_t callback function to indiciate a successful 
+ * operation or failure. on failure program is aborted.
+ * 
+ * @see copt_cb_t;
+ */
+typedef enum 
+{
+    COPT_CB_SUCCESS = 0,    /**< callback exitted with a success. */ 
+    COPT_CB_FAILURE = -1,   /**< callback exitted with a failure. */
+} copt_cb_return_t;
+
+
+/**
+ * When used in tandom with copt_type_t as an identifier, copt_return_t 
+ * speicifies where the result of an option/flag should be stored.
+ * 
+ * @see copt_t
+ * @see copt_cb_t
+ * @see copt_type_t
+ */
+typedef union 
+{
+    void *return_ptr;       /**< unused, allow for indirect pointer casts */
+    char **p_str;           /**< specify a const string return */
+    int *p_int;             /**< specify an integer/boolean return. */
+    copt_cb_t cb_fn;        /**< specify an external callback. */
+} copt_return_t;
+
+
+/**
+ * Defines a parsable option/flag, complete with short, long, return type, and
+ * description. Typically, functions expect an array of options to be provided
+ * each following this format.
+ * 
+ * @see copt_type_t
+ * @see copt_return_t
+ * @see copt_parser()
+ */
 typedef struct copt
 {
-    char short_opt;
-    char *long_opt;
-    copt_type_t type;
-    union
-    {
-        void *return_ptr;
-        char **p_str;
-        int *p_int;
-        int (*cb_fn)(struct copt*, size_t, void*);
-    } m;
-    char *desc;
+    char short_opt;         /**< short option single character. */
+    char *long_opt;         /**< long option string. */
+    copt_type_t type;       /**< option return type. */
+    copt_return_t m;        /**< option return variable/pointer */
+    char *desc;             /**< description of the options */
 } copt_t;
 
 
+/**
+ * Define the template information for a program. including rudimentory 
+ * information such as the name, version, description, and copyright.
+ * Additionally, stores a pointer array options/flags the program can 
+ * accept. Used primarily by copt_help_page andcopt_version_page, acting
+ * as an object to hold all the static information they need.
+ * 
+ * @see copt_t
+ * @see copt_help_page()
+ * @see copt_version_page()
+ */
 typedef struct
 {
-    char *name;
-    char *description;
-    char *version;
+    char *name;                 /**< the program's name. */
+    char *description;          /**< a description of the program. */
+    char *version;              /**< the programs's version. */
 
-    char *help_extra;
-    char *version_extra;
+    char *help_extra;           /**< (optional) print with the help page. */
+    char *version_extra;        /**< (optional) print with the version page. */
 
-    char *copyright_year;
-    char *copyright_holder;
-    char *license_name;
-    char *license_url;
+    char *copyright_year;       /**< year of the program's copyright. */
+    char *copyright_holder;     /**< the name of the copyright holder. */
+    char *license_name;         /**< the name of the license. */
+    char *license_url;          /**< a link to the license. */
 
-    char *issues_url;
-    char *repository_url;
-    char *contact_email;
-    char *contact_name;
+    char *issues_url;           /**< where to send issues/bugreports to. */
+    char *repository_url;       /**< link to the program's source repo. */
+    char *contact_email;        /**< provided contact method. */
+    char *contact_name;         /**< main contact's name. */
 
-    copt_t *options;
-    size_t  option_count;
+    copt_t *options;            /**< an array of parsable options. */
+    size_t  option_count;       /**< lenght of the option array. */
 } copt_prog_t;
 
 
